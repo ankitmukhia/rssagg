c0dbb1481546b95314fd95195c599f42daced65e
ankutmukhia <ankitmukhia157@gmail.com>
2024-12-22 18:42:56 +0530

    adds route handler

---
+++ handler_user.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json" 
+	"fmt"
+	"net/http" 
+	"time"
+	
+	"github.com/google/uuid"
+
+	"github.com/ankitmukhia/rssagg/internal/database"
+)
+
+func (apiCfg *state) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Name string `json:"name"`
+	}
+
+	params := parameters{}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Error parsing JOSN %v", err))
+		return
+	}
+
+	user, err := apiCfg.db.CreateUser(r.Context(), database.CreateUserParams{
+	 	ID: uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name: params.Name,
+	})
+
+	if err != nil {
+		responseWithJson(w, 500, fmt.Sprintf("Couldn't create user %v", err))
+		return
+	}
+
+	responseWithJson(w, 200, user)
+}
--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"log"
 	"os"
+	"database/sql"
 	"net/http"
 
 	"github.com/joho/godotenv"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+ 	 _ "github.com/lib/pq"
+
+	"github.com/ankitmukhia/rssagg/internal/database"
 )
+
+//NOTE:Opened connection to db, and stored it in the state struct. Now I can perform all regular db queries.
+type state struct {
+	db *database.Queries
+}
 
 func main() {
 	godotenv.Load()
@@ -16,6 +25,20 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT not found")
+	}
+
+	db_url := os.Getenv("DB_URL")
+	if db_url == "" {
+		log.Fatal("URL not found")
+	}
+
+	db, err := sql.Open("postgres", db_url)
+	if err != nil {
+		log.Fatal("Unable to connect to db", err)
+	}
+	
+	dbQueries := state{
+		db: database.New(db),
 	}
 
 	r := chi.NewRouter()
@@ -31,6 +54,8 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", dbQueries.handlerCreateUser)
+	log.Printf("dbQueries db %v:",dbQueries.db)
 
 	/* attaches another http.handler as a subrouter: v1/healthz */
 	r.Mount("/v1", v1Router)
