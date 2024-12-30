4608353a68db300f30540ef52f0f90f99d6a740a
ankutmukhia <ankitmukhia157@gmail.com>
2024-12-31 01:41:41 +0530

    adds scraping

---
+++ rss.go
@@ -0,0 +1,52 @@
+// RSS is just structured data in XML format, Kind of like crappy JSON.
+package main
+
+import (
+	"net/http"
+	"time"
+	"encoding/xml"
+	"io"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title string `xml:"title"`
+		Link string `xml:"link"`
+		Description string `xml:"description"`
+		Language string `xml:"language"`
+		Item []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title string `xml:"title"`	
+	Link string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate string `xml:"pubDate"` 
+}
+
+func urlToFeed(url string) (RSSFeed, error) {
+	// fetch every ten second
+	httpClient := http.Client{
+		Timeout: 10 * time.Second,
+	}
+	res, err := httpClient.Get(url)
+	if err != nil {
+		return RSSFeed{}, nil
+	}
+	defer res.Body.Close()
+	
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return RSSFeed{}, nil
+	}
+
+	rssFeed := RSSFeed{}
+
+	err = xml.Unmarshal(data, &rssFeed)
+	if err != nil {
+		return RSSFeed{}, nil
+	}
+	
+	return rssFeed, nil
+}
---
+++ scraper.go
@@ -0,0 +1,12 @@
+package main
+
+// scraper is a long running job, scraper func is going to run in the background as our server run.
+import (
+	"time"
+
+	"github.com/ankitmukhia/rssagg/internal/database"
+)
+
+func startScraping(database *database.Queries, concurrency int, timeBetweenRequest time.Duration) {
+	//?
+}
