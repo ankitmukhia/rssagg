c1f3e3ed9381de38155b56a90e974732102fa036
ankutmukhia <ankitmukhia157@gmail.com>
2025-01-05 21:54:26 +0530

    update

--- sql/queries/posts.sql
+++ sql/queries/posts.sql
@@ -11,3 +11,10 @@ VALUES (
 	$8
 )
 RETURNING *;
+
+-- name: GetPostsForUser :many
+SELECT posts.* from posts
+JOIN feed_follows ON posts.feed_id = feed_follows.feed_id
+WHERE feed_follows.user_id = $1
+ORDER BY posts.published_at DESC
+LIMIT $2;
